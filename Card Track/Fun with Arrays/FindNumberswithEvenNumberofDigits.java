ab3aaf25490a87fd05ff5c016203ba7c8e4f45eb
henry-azer <henryazer@outlook.com>
2024-03-09 23:51:23 +0200

    Solve Card Track - Arrays - Find Numbers with Even Number of Digits

---
+++ Card Track/Fun with Arrays/FindNumberswithEvenNumberofDigits.java
@@ -0,0 +1,22 @@
+package org.henry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {555, 90, 482, 1771, 3990};
+        System.out.println(findNumbers(nums));
+    }
+
+    public static int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            int digits = 1;
+            while (num > 9) {
+                num /= 10;
+                digits +=1;
+            }
+            if (digits % 2 == 0) count +=1;
+        }
+        return count;
+    }
+}
