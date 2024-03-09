ce07f841d69a3a125bff6bf3ef5f1265e814813a
henry-azer <henryazer@outlook.com>
2024-03-09 23:19:00 +0200

    Solve Card Track - Arrays - Max consecutive ones

---
+++ Card Track/Fun with Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,22 @@
+package org.henry;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 0, 1, 1, 1};
+        System.out.println(findMaxConsecutiveOnes(nums));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, temp = 0;
+        for (var num : nums) {
+            if (num == 1) {
+                temp += 1;
+            } else {
+                max = Math.max(max, temp);
+                temp = 0;
+            }
+        }
+        return Math.max(max, temp);
+    }
+}
