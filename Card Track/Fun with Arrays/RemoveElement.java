a9d79114b2a96a44922975d30f7b05a058a88e63
henryazer@outlook.com <henryazer@outlook.com>
2024-05-02 08:57:42 +0300

    Solve Card Track - Arrays - Remove Element

--- Card Track/Fun with Arrays/RemoveElement.java
+++ Card Track/Fun with Arrays/RemoveElement.java
@@ -8,6 +8,10 @@ public class RemoveElement {
         int[] nums = {1, 2, 2, 3, 5, 6, 2, 2, 7, 7, 8};
         int k = removeElement(nums, 2);
         System.out.println(k + ", nums = " + Arrays.toString(nums));
+
+        int[] nums2 = {1, 2, 2, 3, 5, 6, 2, 2, 7, 7, 8};
+        int k2 = removeElement2(nums2, 2);
+        System.out.println(k2 + ", nums = " + Arrays.toString(nums2));
     }
 
     public static int removeElement(int[] nums, int val) {
@@ -20,5 +24,18 @@ public class RemoveElement {
         }
         Arrays.sort(nums);
         return k;
+    }
+
+    public static int removeElement2(int[] nums, int val) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            if (nums[l] == val) {
+                nums[l] = nums[r];
+                nums[r--] = 101;
+            } else {
+                l++;
+            }
+        }
+        return l;
     }
 }
